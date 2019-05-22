61917bca411b062f1a66486250eafa710a871a9e
arsalan <arsalanirfan888@gmail.com>
2019-05-23 01:40:31 +0500

    Working on models and routes

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const {check,validationResult}=require('express-validator/check')
 
-router.get('/',(req,res)=>{
-    res.send('user route');
-})
+router.post('/',[
+    check('name','Name is required').not().isEmpty(),
+    check('email','Please Enter a valid email').isEmail(),
+    check('password','Please enter password with 6 or more characters').isLength({min:6})
+    ],
+    (req,res)=>{
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()})
+        }
+        res.send('user route');
+
+    })
 
 module.exports=router;
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ const app = express();
 //Connect DB
 connectDB();
 
+//Init Middleware
+app.use(express.json({extended:false}));
+
 app.get('/',(req,res)=>{
     res.send("Api running")
 });
