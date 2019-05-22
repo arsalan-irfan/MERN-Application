0870b6bb146c2a791c384b9cee414c9cb106786b
arsalan <arsalanirfan888@gmail.com>
2019-05-23 01:03:01 +0500

    Routes Initialized

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,13 @@ app.get('/',(req,res)=>{
     res.send("Api running")
 });
 
+app.use('/api/users',require('./routes/api/users'))
+app.use('/api/auth',require('./routes/api/auth'))
+app.use('/api/posts',require('./routes/api/posts'))
+app.use('/api/profile',require('./routes/api/profile'))
+
 const PORT = process.env.PORT || 5000;
+
+
 
 app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`) );
