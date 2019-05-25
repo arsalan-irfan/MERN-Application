4a7d904dbf972d047ab6bde968a572c5e6d9bbfb
arsalan <arsalanirfan888@gmail.com>
2019-05-25 16:32:10 +0500

    db modified

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,22 +2,21 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDb = async ()=>{
-    try{
+const connectDb = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
 
-        await mongoose.connect(db,{
-            useNewUrlParser:true,
-            useCreateIndex:true
-        })
+    console.log('Mongodb connected');
+  } catch (err) {
+    console.log(err.message);
 
-        console.log("Mongodb connected");
-    }
-    catch(err){
-        console.log(err.message);
+    //Exit process with failure
+    process.exit(1);
+  }
+};
 
-        //Exit process with failure
-        process.exit(1);
-    }
-}
-
-module.exports=connectDb
+module.exports = connectDb;
