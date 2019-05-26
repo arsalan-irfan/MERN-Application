307bba8e0dee0e71fd5102d1095ae5d56ec1130a
arsalan <arsalanirfan888@gmail.com>
2019-05-27 02:27:14 +0500

    Profile Route In process

--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -237,4 +237,88 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
   }
 });
 
+// @route    PUT api/profile/education
+// @desc     Add profile education
+// @access   Private
+
+router.put(
+  '/education',
+  [
+    auth,
+    [
+      check('school', 'School name is required')
+        .not()
+        .isEmpty(),
+      check('fieldofstudy', 'Field is required')
+        .not()
+        .isEmpty(),
+      check('Degree', 'Degree is required')
+        .not()
+        .isEmpty(),
+      check('from', 'from date is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors) {
+      res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    };
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.education.unshift(newEdu);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+// @route    Delete api/profile/experience/:exp_id
+// @desc     Delete profile experience
+// @access   Private
+
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    removeIndex = profile.education
+      .map(item => item.id)
+      .indexOf(req.param.edu_id);
+
+    profile.education.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
